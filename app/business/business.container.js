960b5f5a826921f671425180a86a2596cd25e4ee
Dominik <kamyka100@gmail.com>
2021-05-31 08:44:36 +0200

    server done

--- app/business/business.container.js
+++ app/business/business.container.js
@@ -1,14 +1,19 @@
 'use strict';
 
 import userManager from './user.manager';
+import postManager from './post.manager';
+import postEndpoint from "../REST/post.endpoint";
 
 
 function getter(manager, request) {
-  return function () {
-    return manager.create(request, this);
-  };
+    return function () {
+        return manager.create(request, this);
+    };
 }
 
 export default {
     getUserManager: getter(userManager),
+    getPostManager: getter(postManager),
+    getPostEndpoint: getter(postEndpoint)
 };
+
